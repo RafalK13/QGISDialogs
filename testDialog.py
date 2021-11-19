46ad02ec5961705d326c5fc0a1f8e585c13970ef
Rafał Karczewski <rkarczewski@GIWK.lokalne>
2021-11-19 08:39:00 +0100

    added comparing dirPaths

--- testDialog.py
+++ testDialog.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # from PyQt5.QtWidgets import *
-from qgis.PyQt.QtWidgets import QLineEdit, QDateEdit, QDialog, QCheckBox
+from qgis.PyQt.QtWidgets import QLineEdit, QDateEdit, QDialog, QCheckBox, QDialogButtonBox
 from qgis.PyQt.QtCore import QDate
 # from PyQt5.QtWidgets import QLineEdit //zamienie
 from datetime import *
@@ -13,58 +13,34 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
-# def fun1(dialog, layer, feature):
-    
-#     nameField = dialog.findChild( QLineEdit,"date1")
-    
-#     nameField.setText("")
-#     if( nameField ):
-#         nameField.editingFinished.connect(lambda: nameFieldChenged( nameField.text(), dialog))
-   
-  
-# def nameFieldChenged( tekstNew, dialog):
-   
-#     nameField = dialog.findChild( QLineEdit,"date1")
-
-#     try:
-#         if( tekstNew != ""):
-#             newDate = parser.parse(tekstNew)
-#             nameField.setText("{}-{}-{}".format( newDate.year, newDate.month, newDate.day))
-        
-#     except parser.ParserError as blad:
-#         iface.messageBar().pushWarning( "Błąd", "Zły format daty")
-#         nameField.setText("");
-
-#     except: 
-#         newDate = None
-#         iface.messageBar().pushCritical( "Błąd", "Tell Rafał about it")
-#         nameField.setText("");
-
 def fun1(dialog, layer, feature):
     
     c1 = NewDataClass( dialog, "date1")
     c2 = NewDataClass( dialog, "date2")
-    
+  
     umBezterm = dialog.findChild( QCheckBox, "ter")
     if( umBezterm):
         umBezterm.stateChanged.connect(lambda: umBeztermChanged() )
         
-    # dirPath = dialog.findChild( QgsFileWidget, "dirPath")
-    # if( dirPath):
-    #     dirPath.fileChanged.connect(lambda: dirPathChanged() )
-        
-    # def dirPathChanged():
-    #     oldPath = dirPath.filePath()
-    #     dirPath.setFilePath( "file:///{}".format(oldPath))       
+    def buttonAccepted():
+        dirPath = dialog.findChild( QgsFileWidget, "dirPath")
+
+        if( dirPath.filePath() == '\\\\kajko\\EKSPLOATACJA'):
+            dirPath.setFilePath('') 
     
+    button = dialog.findChild( QDialogButtonBox, "buttons")
+    
+    if( button):
+        button.accepted.connect( buttonAccepted);
+
     def umBeztermChanged( ):
         if( umBezterm.isChecked()):
             c2.tekstElem.setText("")
             c2.tekstElem.setEnabled(False)
-            iface.messageBar().pushCritical( "CheckBox", "Is checked")
+            # iface.messageBar().pushCritical( "CheckBox", "Is checked")
         else:
             c2.tekstElem.setEnabled(True)
-            iface.messageBar().pushCritical( "CheckBox", "Is not checked")
+            # iface.messageBar().pushCritical( "CheckBox", "Is not checked")
            
 class NewDataClass:
     def __init__(self, dialog, elementName):
